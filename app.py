807a578febf9caa6e3170c7e3ddb6f5e081adca7
Pedro <96pedroelias96@gmail.com>
2023-08-29 21:32:47 -0500

    comit para incluir videos

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-# Import necessary libraries
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
 from PIL import Image
+from typing import List
 from io import BytesIO
+import shutil
+from pathlib import Path
 import os
-from google_drive_downloader import GoogleDriveDownloader as gdd
-from clip_interrogator import Config, Interrogator
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+from clip_interrogator import Config, Interrogator  # Ensure this package is installed
 
 # Initialize the FastAPI app
 app = FastAPI()
@@ -18,65 +17,54 @@ config.clip_model_name = 'ViT-L-14/openai'
 config.caption_model_name = 'blip-large'
 ci = Interrogator(config)
 
-# Define a function to process images and generate prompts
+# Directory to store uploaded videos
+video_directory = "uploaded_videos"
+
+# Create the directory if it doesn't exist
+Path(video_directory).mkdir(parents=True, exist_ok=True)
+
+# Function to process images and generate prompts
 def image_to_prompt(image, mode):
     prompt = ci.get_prompt_from_image(image, mode=mode)
     return prompt
 
-# Define a function to process videos and generate prompts
+# Function to process videos and generate prompts
 def video_to_prompt(video_path, mode):
-    # Process video and get its analysis results
     with open(video_path, "rb") as video_file:
         video_data = video_file.read()
     video_stream = BytesIO(video_data)
     video_image = Image.open(video_stream)
-    prompt = image_to_prompt(video_image, mode=mode)  # Modify the mode as needed
+    prompt = image_to_prompt(video_image, mode=mode)
     return prompt
 
-# Initialize Google Drive API
-api_service_name = "drive"
-api_version = "v3"
-creds = None  # Use your credentials here
-service = build(api_service_name, api_version, credentials=creds)
+# Endpoint to upload videos
+@app.post("/uploadvideos/")
+async def upload_videos(files: List[UploadFile] = File(...)):
+    file_names = []
+    for file in files:
+        file_names.append(file.filename)
+        path = Path(video_directory) / file.filename
+        with path.open("wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+    return {"filenames": file_names}
 
-# Define a route to process videos
+# Endpoint to process uploaded videos
 @app.post("/api/process_videos/")
-async def process_videos(folder_info: dict):
+async def process_videos():
     try:
-        # Extract the folder URL from the request
-        folder_url = folder_info.get("folder_url")
-        if not folder_url:
-            raise HTTPException(status_code=400, detail="Folder URL is missing in the request")
-
-        # Extract the folder ID from the URL
-        folder_id = folder_url.split("/")[5].split("?")[0]
-
-        # List video filenames in the Google Drive folder using Google Drive API
-        query = f"'{folder_id}' in parents and mimeType='video/mp4'"
-        response = service.files().list(q=query, fields="files(name)").execute()
-        video_filenames = [file.get("name") for file in response.get("files", [])]
-
-        # Process the videos and return results
+        video_filenames = list(Path(video_directory).glob("*.mp4"))  # Scan for .mp4 files in the upload directory
         results = []
         for video_filename in video_filenames:
             try:
-                # Download video data from Google Drive
-                video_path = os.path.join('videos', video_filename)
-                download_url = f"https://drive.google.com/uc?id={video_filename}"
-                os.system(f"wget {download_url} -O {video_path}")
-    
-                # Process video and get its analysis results
-                prompt = video_to_prompt(video_path, mode="best")  # Modify the mode as needed
-                analysis_result = {"video": video_filename, "prompt": prompt}
+                prompt = video_to_prompt(video_filename, mode="best")
+                analysis_result = {"video": video_filename.name, "prompt": prompt}
                 results.append(analysis_result)
             except Exception as e:
-                results.append({"video": video_filename, "error": str(e)})
-
+                results.append({"video": video_filename.name, "error": str(e)})
         return JSONResponse(content={"results": results})
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
-# Run the app using Gunicorn on port 4000
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=4000)
