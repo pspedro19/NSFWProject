f13b093140e65161e5954e614dc56050415a8c9d
Pedro <96pedroelias96@gmail.com>
2023-08-31 20:01:41 -0500

    creating a good description

--- app.py
+++ app.py
@@ -1,70 +1,127 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from fastapi.responses import JSONResponse
-from PIL import Image
+from fastapi import FastAPI, File, UploadFile
 from typing import List
-from io import BytesIO
+import json
+import uvicorn
 import shutil
 from pathlib import Path
+import random
+from moviepy.editor import VideoFileClip
+from PIL import Image
+from clip_interrogator import Config, Interrogator
 import os
-from clip_interrogator import Config, Interrogator  # Ensure this package is installed
-
-# Initialize the FastAPI app
-app = FastAPI()
-
-# Initialize the Clip Interrogator configuration and Interrogator
+import requests
+# Configurar el modelo de IA
 config = Config()
 config.clip_model_name = 'ViT-L-14/openai'
 config.caption_model_name = 'blip-large'
 ci = Interrogator(config)
 
-# Directory to store uploaded videos
-video_directory = "uploaded_videos"
+app = FastAPI()
 
-# Create the directory if it doesn't exist
-Path(video_directory).mkdir(parents=True, exist_ok=True)
+async def extract_screenshots(video_path: Path, num_screenshots: int = 1):
+    clip = VideoFileClip(str(video_path))
+    duration = clip.duration
+    text_list = []
 
-# Function to process images and generate prompts
-def image_to_prompt(image, mode):
-    prompt = ci.get_prompt_from_image(image, mode=mode)
-    return prompt
+    for i in range(num_screenshots):
+        time = random.uniform(0, duration)
+        screenshot_path = Path(f"{video_path.stem}screenshot{i + 1}.png")
+        clip.save_frame(screenshot_path, t=time)
 
-# Function to process videos and generate prompts
-def video_to_prompt(video_path, mode):
-    with open(video_path, "rb") as video_file:
-        video_data = video_file.read()
-    video_stream = BytesIO(video_data)
-    video_image = Image.open(video_stream)
-    prompt = image_to_prompt(video_image, mode=mode)
-    return prompt
+        # Convertir la imagen a texto usando tu modelo
+        image = Image.open(screenshot_path).convert('RGB')
+        text = ci.interrogate(image)  # Utilizando tu función 'interrogate'
+        text_list.append(text)
 
-# Endpoint to upload videos
+    return text_list
+
 @app.post("/uploadvideos/")
-async def upload_videos(files: List[UploadFile] = File(...)):
+async def upload_videos(
+    files: List[UploadFile] = File(...),
+    studio: str = Form(...),
+    pornstars: str = Form(...),
+    keywords: str = Form(...)
+):
     file_names = []
+    texts = {}
+
     for file in files:
         file_names.append(file.filename)
-        path = Path(video_directory) / file.filename
+
+        # Crear un Path para el archivo en el directorio raíz
+        path = Path(file.filename)
+
+        # Escribir el archivo en el sistema de archivos
         with path.open("wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
-    return {"filenames": file_names}
 
-# Endpoint to process uploaded videos
-@app.post("/api/process_videos/")
-async def process_videos():
-    try:
-        video_filenames = list(Path(video_directory).glob("*.mp4"))  # Scan for .mp4 files in the upload directory
-        results = []
-        for video_filename in video_filenames:
-            try:
-                prompt = video_to_prompt(video_filename, mode="best")
-                analysis_result = {"video": video_filename.name, "prompt": prompt}
-                results.append(analysis_result)
-            except Exception as e:
-                results.append({"video": video_filename.name, "error": str(e)})
-        return JSONResponse(content={"results": results})
-    except Exception as e:
-        return JSONResponse(content={"error": str(e)}, status_code=500)
+        # Extraer 5 capturas de pantalla aleatorias del video y convertirlas a texto
+        text_list = await extract_screenshots(path)
+        texts[file.filename]= text_list
+    texto = texts[file.filename][0]
+    prompt = "Give me a coherence video description for a sex porn video use explicit words the name of the porn model and context to make up the video descriptions, souds dirty and use all vocabulary related for NFSW for sex and porn escenes"
+    prompt = prompt + "############  use this context to create the description, use only the most important words and make up the porn video description: " +  texto + " ################## Pornstarname:"+ pornstars + "###################### Include in the description the following key words:" + keywords 
+    HOST = 'localhost:5000'
+    URI = f'http://{HOST}/api/v1/chat'
+    history = {'internal': [], 'visible': []}
+    request = {
+        'user_input': prompt,
+        'max_new_tokens': 250,
+        'auto_max_new_tokens': False,
+        'max_tokens_second': 0,
+        'history': history,
+        'mode': 'instruct',  # Valid options: 'chat', 'chat-instruct', 'instruct'
+        'character': 'Example',
+        'instruction_template': 'Vicuna-v1.1',  # Will get autodetected if unset
+        'your_name': 'You',
 
-if __name__ == "__main__":
-    import uvicorn
+        # Generation params. If 'preset' is set to different than 'None', the values
+        # in presets/preset-name.yaml are used instead of the individual numbers.
+        'preset': 'None',
+        'do_sample': True,
+        'temperature': 0.7,
+        'top_p': 0.1,
+        'typical_p': 1,
+        'epsilon_cutoff': 0,  # In units of 1e-4
+        'eta_cutoff': 0,  # In units of 1e-4
+        'tfs': 1,
+        'top_a': 0,
+        'repetition_penalty': 1.18,
+        'repetition_penalty_range': 0,
+        'top_k': 40,
+        'min_length': 0,
+        'no_repeat_ngram_size': 0,
+        'num_beams': 1,
+        'penalty_alpha': 0,
+        'length_penalty': 1,
+        'early_stopping': False,
+        'mirostat_mode': 0,
+        'mirostat_tau': 5,
+        'mirostat_eta': 0.1,
+        'guidance_scale': 1,
+        'negative_prompt': '',
+
+        'seed': -1,
+        'add_bos_token': True,
+        'truncation_length': 2048,
+        'ban_eos_token': False,
+        'skip_special_tokens': True,
+        'stopping_strings': []
+    }
+
+    response = requests.post(URI, json=request)
+
+    if response.status_code == 200:
+        result = response.json()['results'][0]['history']
+
+    result = response.json()['results'][0]['history']
+    otro = print(json.dumps(result, indent=4))
+    otro = str(otro)
+    print(json.dumps(result, indent=4))
+    print(prompt)
+    print(str(result))
+    result = str(result)
+    return { "Description": result}
+
+if name == "main":
     uvicorn.run(app, host="0.0.0.0", port=4000)
